fc6d65d77ed58b5d8cb59cdaeb3a8f75e585dbc1
67-vincent <team.alberico@gmail.com>
2026-03-06 11:28:22 -0800

    Update 131artproject.py

--- 131artproject.py
+++ 131artproject.py
@@ -1,3 +1,4 @@
+#imports
 import turtle as trtl
 import random as rand
 import leaderboard as lb
@@ -15,32 +16,32 @@ panda_image = "panda1.gif"
 wn.addshape(heart_image)
 wn.addshape(panda_image)
 wn.tracer(0)
-
+# leaderbaord variables
 leaderboard_file_name = "131leaderboard.txt"
 player_name = wn.textinput("Player Name", "Enter your name:") # Get name at start
-
+#variables
 timer = 5
 score = 0
 timer_up = False
 font_setup = ("Arial", 18, "bold")
-message_font = ("Arial", 14, "italic") 
+message_font = ("Arial", 14, "bold") 
 
 #-----initialize turtles-----
 
-# 1. Background Heart Stamper
+#Background Heart Stamper leaves the hearts on screen
 stamper = trtl.Turtle()
 stamper.hideturtle() 
 stamper.shape(heart_image)
 stamper.penup()
 stamper.speed(0)
 
-# 2. Border Drawer
+#Border Drawer
 border = trtl.Turtle()
 border.hideturtle()
 border.speed(0)
 border.pensize(2)
 
-# 3. The Panda
+#The Panda
 panda = trtl.Turtle()
 panda.penup()
 panda.shape(panda_image)
@@ -51,31 +52,32 @@ string_drawer = trtl.Turtle()
 string_drawer.hideturtle()
 string_drawer.speed(0)
 
-# 5. The Main Heart Balloon (Target)
+# 5. The Main Heart Balloon
 balloon = trtl.Turtle()
 balloon.shape(heart_image)
 balloon.penup()
 balloon.goto(0, 100)
 
 # 6. UI Turtles
+#ui that writes the score
 score_writer = trtl.Turtle()
 score_writer.hideturtle()
 score_writer.penup()
 score_writer.goto(115, 215)
-
+# ui that writes the timer
 counter = trtl.Turtle()
 counter.hideturtle()
 counter.penup()
 counter.goto(-170, 215)
 
-# 7. Bottom Message Turtle
+# ui that writes the name of the turtle game
 text_writer = trtl.Turtle()
 text_writer.hideturtle()
 text_writer.penup()
 text_writer.goto(0, -230) 
 
 #-----functions--------
-
+# draws the outline design things
 def draw_design():
     border.penup()
     border.goto(-180, 250)
@@ -87,12 +89,11 @@ def draw_design():
         for _ in range(16):
             border.forward(15); border.penup(); border.forward(15); border.pendown()
         border.right(90)
-
+# function writing the game name on the bottom
 def write_bottom_message():
     text_writer.clear()
-    # Change "Panda Game" to whatever you want later!
     text_writer.write("Panda Game", align="center", font=message_font)
-
+# draws the balloon string and moves it to new x coords
 def draw_string():
     string_drawer.clear()
     if not timer_up:
@@ -101,13 +102,13 @@ def draw_string():
         string_drawer.pendown()
         string_drawer.pensize(2)
         string_drawer.goto(balloon.xcor(), balloon.ycor())
-
+# updates the score +1
 def update_score(): #score update
   global score
   score += 1
   score_writer.clear()
   score_writer.write(score, font=font_setup)
-
+# stamper goes to x coord, stamps, moves to new x coord and updates hwen clicked
 def move_balloon():
     if not timer_up:
         # 1. Use STAMPER to leave the mark (keeps main balloon clickable)
@@ -121,7 +122,7 @@ def move_balloon():
         
         draw_string()
         wn.update()
-
+# coundown
 def countdown():
     global timer, timer_up
     counter.clear()
@@ -133,7 +134,7 @@ def countdown():
         counter.write(f"Time: {timer}", font=font_setup)
         timer -= 1
         wn.ontimer(countdown, 1000)
-
+# leaderboard, calls to the leaderboard.py file
 def manage_leaderboard():
     global score
     # 1. Load current leaderboard
